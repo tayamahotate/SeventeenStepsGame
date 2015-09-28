f59c18c4e1e9ec177d79b78d6300808f73025ec8
hotatekaoru <taymahotate@gmail.com>
2015-09-29 01:12:34 +0900

    DealHai

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -3,36 +3,80 @@
 
 #include <random>
 
-#define HAI_NUM 4          // 1種類あたりの牌の数
-#define HAI_TYPE_NUM 34    // 牌の種類
-#define TEHAI_NUM 13       // 手牌の枚数
 
-#define HAI_1_POS_X 50 //1番の牌の位置(x)
-#define HAI_1_POS_Y 35 //1番の牌の位置(y)
-#define HAI_DISTANCE_X 23 //牌の間の距離(x方向)
+#define HAI_NUM 4           // 1種類あたりの牌の数
+#define HAI_TYPE_NUM 34     // 牌の種類
+
+#define AKAHAI_NUM_MAN 1    // 赤5萬の枚数
+#define AKAHAI_NUM_PIN 2    // 赤5筒の枚数
+#define AKAHAI_NUM_SOU 1    // 赤5索の枚数
+
+#define HAIPAI_NUM_X 12     // 配牌の枚数(x)
+#define HAIPAI_NUM_Y 3      // 配牌の枚数(y)
+#define HAIPAI_1_POS_X 20   // 配牌の1番の牌の位置(x)
+#define HAIPAI_1_POS_Y 90   // 配牌の1番の牌の位置(y)
+
+#define HAI_DISTANCE_X 23   // 牌の間の距離(x方向)
+#define HAI_DISTANCE_Y 40   // 牌の間の距離(y方向)
+
+#define TEHAI_NUM_X 13      // 手牌の枚数(x)
+#define TEHAI_1_POS_X 50    // 1番の牌の位置(x)
+#define TEHAI_1_POS_Y 35    // 1番の牌の位置(y)
 
 USING_NS_CC;
 
-Scene* GameScene::createScene() {
-    //シーンを生成する
-    auto scene = Scene::create();
-    //HelloWorldクラスのレイヤーを生成する
-    auto layer = GameScene::create();
-    //シーンに対してレイヤーを追加する
-    scene->addChild(layer);
-    //シーンを返す
-    return scene;
-
-return scene;
+/* 牌クラスの初期化 */
+bool HaiSprite::init() {
+    if (!Sprite::init()) {
+        return false;
+    }
+    return true;
 }
 
-// 初期化
+/* 牌の表示前処理 */
+void HaiSprite::onEnter() {
+
+    Sprite::onEnter();
+    /* 画像の表示 */
+    setTexture(HaiSprite::getFileName(_hai));
+    
+    /* 牌の位置とタグを指定 */
+    float posX = HAIPAI_1_POS_X + HAI_DISTANCE_X * _posIndex.x;
+    float posY = HAIPAI_1_POS_Y + HAI_DISTANCE_Y * _posIndex.y;
+    setPosition(posX, posY);
+    // タグは各牌を一意に特定する1以上の値を設定する（setInitHaisのタグと合わせる）
+    setTag(_posIndex.x + _posIndex.y * HAIPAI_NUM_X + 1);
+}
+
+/* ソート処理 */
+/*
+bool GameScene::sortHai(const Hai& hai1, const Hai& hai2) {
+    if (hai1.type < hai2.type) {
+        return true;
+    } else if (hai1.type == hai2.type && hai1.number < hai2.number) {
+        return true;
+    } else {
+        return false;
+    }
+}
+*/
+
+/* GameSceneの作成 */
+Scene *GameScene::createScene() {
+
+    auto scene = Scene::create();
+    auto layer = GameScene::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+/* GameSceneの初期化 */
 bool GameScene::init() {
 
-    /* お約束 */
     if ( !Layer::init() ) {
         return false;
     }
+
     /* 背景画像設定 */
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
@@ -47,12 +91,16 @@ bool GameScene::init() {
     
     /* ゲームを初期化する */
     GameScene::initGame();
+    
+    /* 配牌を行う */
+    
 
     return true;
 }
 
 /* 右サイドのメニュー設定 */
 void GameScene::showRightSide() {
+
     /* 背景 */
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
@@ -86,84 +134,170 @@ void GameScene::showRightSide() {
 
 /* ReturnButton押下時の処理 */
 void GameScene::pushReturnButton(Ref* pSender) {
-    Director::getInstance()->pushScene(TransitionFade::create(1.0f, FirstMenu::createScene(), Color3B::BLACK));
+
+    Director::getInstance()->pushScene(TransitionFade::create(
+            1.0f, FirstMenu::createScene(), Color3B::BLACK));
 }
 
 /* ゲーム初期化 */
 void GameScene::initGame() {
+
     /* 牌を初期化する */
     GameScene::initHais();
     
-    /* 牌を表示する */
-    GameScene::showInitHais();
+    /* 配牌 */
+    GameScene::dealHais();
+
 }
 
 /* 牌の初期化 */
 void GameScene::initHais() {
+    
     /* 牌のクリア */
     _hais.clear();
     
     /* ランダムに配牌 */
-    //34種類 x 4枚分（後々の役の計算を考慮して1からループを回す）
+    //34種類 x 4枚分（ファイル名の取得や役の計算を考慮して1からループを回す）
     for (int type = 1; type <= HAI_TYPE_NUM; type++) {
         // 牌の種類も上記に合わせて、1からループを回す
         for (int number = 1; number <= HAI_NUM; number++) {
-            //カード情報の作成
+            //牌情報の作成
             Hai hai;
             hai.number = number;
             hai.type = type;
             
-            //カードを追加する
+            //牌を追加する
             _hais.push_back(hai);
         }
     }
 }
 
+/* 牌の設定 */
+void GameScene::dealHais() {
+
+    cocos2d::Sprite hai;
+    _tehai.clear();
+
+    /* 牌のタグを初期化 */
+    for (int tag = 1; tag <= HAI_NUM * HAI_TYPE_NUM; tag++) {
+        auto hai = getChildByTag(tag);
+        if (hai) {
+            // 牌が残っている場合は、削除する
+            hai->removeFromParent();
+        }
+    }
+    
+    /* 配牌の枚数分、牌の位置を指定する */
+    for (int y = 0; y < HAIPAI_NUM_Y; y++) {
+        for (int x = 0; x < HAIPAI_NUM_X; x++) {
+            PosIndex posIndex;
+            posIndex.x = x;
+            posIndex.y = y;
+    
+            //牌の生成
+            createHai(posIndex);
+        }
+    }
+    
+    /* 牌のソート */
+/*    for (int count = 0; count < 34; count++) {
+        _tehai.push_back(_hais[count]);
+    }
+    
+    _tehai.sort();
+*/
+}
+
+/* 牌の生成と指定 */
+void GameScene::createHai(PosIndex posIndex) {
+
+    auto hai = HaiSprite::create();
+    
+    // 牌をランダムに生成
+    hai->setHai(getHai());
+    // 順々に位置を設定
+    hai->setPosIndex(posIndex);
+    addChild(hai, 1);
+}
+
 /* 牌の取得 */
 Hai GameScene::getHai() {
+    
     std::random_device rd;
     std::mt19937 rand = std::mt19937(rd());
     
-    //インデックスをランダムに取得する
+    /* インデックスをランダムに取得する */
     int index = std::uniform_int_distribution<int>(0, (int)_hais.size() - 1)(rand);
     
-    //カードの数値を取得する
+    /* 牌の数値を取得する */
     auto hai = _hais[index];
     
-    //一時カードから削除する
+    /* 牌がかぶらないように、一時牌から削除する */
     _hais.erase(_hais.begin() + index);
     
     return hai;
 }
 
-/* 牌の表示 */
-void GameScene::showInitHais() {
-    for (int tag = 1; tag <= 10; tag++) {
-        auto hai = getChildByTag(tag);
-        if (hai) {
-            //カードが残っている場合は、削除する
-            hai->removeFromParent();
+/* 牌の画像表示 */
+std::string HaiSprite::getFileName(Hai hai) {
+
+    std::string fileName;       // 牌の色と数から判定されたファイル名
+    std::string fileFirstName;  // 牌の色を表す
+    std::string fileSecondName; // 牌の数を表す
+
+    /* 牌の色判定 */
+    switch (hai.type) {
+        case 1: case 2: case 3: case 4: case 5:
+        case 6: case 7: case 8: case 9:
+            fileFirstName = "wz"; break;
+        case 10: case 11: case 12: case 13: case 14:
+        case 15: case 16: case 17: case 18:
+            fileFirstName = "pz"; break;
+        case 19: case 20: case 21: case 22: case 23:
+        case 24: case 25: case 26: case 27:
+            fileFirstName = "sz"; break;
+        default:
+            fileFirstName = "kz"; break;
+    }
+    
+    /* 赤牌判定 */
+    fileSecondName = checkAkaHai(hai);
+    
+    /* 牌の数判定 */
+    if (fileSecondName == "0") {
+        fileSecondName = StringUtils::format("%d", hai.type % 9);
+        // 9で割った余りが0の牌（9萵など）はfileSecondNameに9を指定する
+        if (fileSecondName == "0") {
+            fileSecondName = "9";
         }
     }
     
-    /* 枚数分生成 */
-    for (int x = 0; x < TEHAI_NUM; x++) {
-        PosIndex posIndex;
-        posIndex.x = x;
-            
-        //牌の生成
-        createHai(posIndex);
-    }
+    fileName = "haiImage/" + fileFirstName + fileSecondName + ".png";
+
+    return fileName;
 }
 
-/* 牌の生成 */
-void GameScene::createHai(PosIndex posIndex) {
-    //新しいカードを作成する
-    auto hai = Sprite::create("haiImage/wz1.png");
-    hai->setPosition(HAI_1_POS_X + HAI_DISTANCE_X * posIndex.x,
-                      HAI_1_POS_Y);
-    addChild(hai, 1);
+/* 赤牌判定 */
+/* 赤牌は"10"以外は、"0"を返却する。 */
+std::string HaiSprite::checkAkaHai(Hai hai) {
+
+    std::string fileSecondName = "0"; // 牌の数を表す
+    /* 赤5萵判定 */
+    if (hai.type == 5 && hai.number <= AKAHAI_NUM_MAN) {
+        fileSecondName = "10";
+    }
+    /* 赤5筒判定 */
+    if (hai.type == 14 && hai.number <= AKAHAI_NUM_PIN) {
+        fileSecondName = "10";
+    }
+    /* 赤5索判定 */
+    if (hai.type == 23 && hai.number == AKAHAI_NUM_SOU) {
+        fileSecondName = "10";
+    }
+    
+    return fileSecondName;
 }
+
 
 
 
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -2,29 +2,60 @@
 #define __SeventeenSteps__Game__
 
 #include "cocos2d.h"
+#include <algorithm>
+#include <list>
+#include <functional>
 
-
-//牌
+using namespace std;
+// 牌情報
 struct Hai {
     int type;   // 34種類の牌を表す
     int number; // 1〜4、赤牌を表すために用意
+    
+    bool operator < (const Hai& sortObj) const {
+        if (type != sortObj.type) {
+            return type < sortObj.type;
+        } else {
+            return number < sortObj.number;
+        }
+    }
 };
 
-//カードを表示する位置
+// 牌を表示する位置
 struct PosIndex {
     int x;
+    int y;
 };
+
+// 牌のスプライト
+class HaiSprite : public cocos2d::Sprite {
+protected:
+    std::string getFileName(Hai hai); //画像ファイル名取得
+    std::string checkAkaHai(Hai hai); //赤牌判定
+    
+public:
+    virtual bool init(); // 初期化処理
+    void onEnter() override; // 表示前処理
+    CREATE_FUNC(HaiSprite); // create関数作成マクロ
+    
+    CC_SYNTHESIZE(Hai, _hai, Hai); // 牌情報
+    CC_SYNTHESIZE(PosIndex, _posIndex, PosIndex); // 表示位置
+};
+
+
 
 class GameScene : public cocos2d::Layer {
 
 protected:
-    std::vector<Hai> _hais; //牌情報
+    std::vector<Hai> _hais; // 牌情報（vectorは、インデックスで管理しているためアクセスが早い）
+    std::list<Hai> _tehai;
     
-    void initGame(); //ゲームを初期化する
-    void initHais(); //牌を初期化する
-    Hai getHai(); //牌を取得する
-    void showInitHais(); //ゲーム開始時にカードを13枚表示する
+    void initGame();    // ゲームを初期化する
+    void initHais();    // 牌を初期化する
+    Hai getHai();       // 牌を取得する
+    void dealHais();    // 配牌
     void createHai(PosIndex posIndex); //牌を作成する
+    void showTehai();   // 手牌を表示する
 
 public:
     // 初期表示
